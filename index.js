07a3d7d35af618ebb58c2a4a93508dc4dd2a351d
Trever Andersen <trever.r.andersen@gmail.com>
2018-01-22 17:10:13 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 // Write your code in this file!
 const currentUser = 'Grace Hopper';
 const welcomeMessage = 'Welcome to Flatbook, ' + currentUser + '!';
-const excitedWelcomeMessage = 'WELCOME TO FLATBOOK, ' + currentUser.uppercase + '!';
+const excitedWelcomeMessage = 'WELCOME TO FLATBOOK, ' + currentUser.toUpperCase(); + '!';
 const shortGreeting = 'Welcome, ' + currentUser + '!';
